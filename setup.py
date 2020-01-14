f474cbd598ad7d73984811840f778d826ad48795
Tom de Geus <tom@geus.me>
2020-01-14 13:09:16 +0100

    Fixing setup

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from setuptools import setup
 import re
 
-filepath = 'pdfcombine/__init__.py'
+filepath = 'pdfshrink/__init__.py'
 __version__ = re.findall(r'__version__ = \'(.*)\'', open(filepath).read())[0]
 
 setup(
-    name = 'pdfcombine',
+    name = 'pdfshrink',
     version = __version__,
     license = 'MIT',
     author = 'Tom de Geus',
     author_email = 'tom@geus.me',
-    description = 'Script of combine PDF files (wrapper around GhostScript)',
-    long_description = 'Script of combine PDF files (wrapper around GhostScript)',
+    description = 'Script of shrink PDF files (wrapper around GhostScript)',
+    long_description = 'Script of shrink PDF files (wrapper around GhostScript)',
     keywords = 'PDF, GhostScript',
-    url = 'https://github.com/tdegeus/pdfcombine',
-    packages = ['pdfcombine'],
+    url = 'https://github.com/tdegeus/pdfshrink',
+    packages = ['pdfshrink'],
     install_requires = ['docopt>=0.6.2', 'click>=4.0'],
     entry_points = {
-        'console_scripts': ['pdfcombine = pdfcombine:main']})
+        'console_scripts': ['pdfshrink = pdfshrink:main']})
